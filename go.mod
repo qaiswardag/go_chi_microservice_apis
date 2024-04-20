ad21b59c0dff6420587976c5fc2c6cea04e62956
Qais Wardag <qais.wardag@outlook.com>
2024-04-20 23:27:53 +0200

    Implement basic HTTP server listener and handler func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qaiswardag/go_chi_microservice_apis
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	server := &http.Server{
+		Addr: ":2020",
+		// handler function runs when the server receives a request
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("Server failed to start: ", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
