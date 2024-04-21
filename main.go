1dc8b0803b918607f3a1dac863e95bcf26c2fb61
Qais Wardag <qais.wardag@outlook.com>
2024-04-21 23:13:03 +0200

    Remove Chi router and middleware

--- main.go
+++ main.go
@@ -3,25 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	// Create a new router using chi package
-	router := chi.NewRouter()
-
-	// Use the Logger middleware for logging requests
-	router.Use(middleware.Logger)
-
-	// Define a route and handle the route with a function
-	router.Get("/", basicHandler)
 
 	// Create an HTTP server with the specified address and router as the handler
 	server := &http.Server{
 		Addr:    ":2020",
-		Handler: router,
+		Handler: http.HandlerFunc(basicHandler),
 	}
 
 	// Start the server and listen for incoming requests
