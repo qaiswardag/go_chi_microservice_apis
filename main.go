ff83d7edb948ab22df6ddac8e3141c6bb9e303ef
Qais Wardag <qais.wardag@outlook.com>
2024-04-21 23:04:58 +0200

    Add comments

--- main.go
+++ main.go
@@ -9,24 +9,32 @@ import (
 )
 
 func main() {
+	// Create a new router using chi package
 	router := chi.NewRouter()
+
+	// Use the Logger middleware for logging requests
 	router.Use(middleware.Logger)
 
-	router.Get("/hello", basicHandler)
+	// Define a route and handle the route with a function
+	router.Get("/", basicHandler)
+
+	// Create an HTTP server with the specified address and router as the handler
 	server := &http.Server{
-		Addr: ":2020",
-		// handler function runs when the server receives a request
+		Addr:    ":2020",
 		Handler: router,
 	}
 
+	// Start the server and listen for incoming requests
 	err := server.ListenAndServe()
 
+	// Print an error message if the server fails to start
 	if err != nil {
 		fmt.Println("Server failed to start: ", err)
 	}
 }
 
 // handle all requests
+// Respond to HTTP requests with for example a message
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	w.Write([]byte("Welcome to the home page!"))
 }
