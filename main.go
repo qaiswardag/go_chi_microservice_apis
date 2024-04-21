17c5d15e108c1cd41805586595f1b24168b1dadf
Qais Wardag <qais.wardag@outlook.com>
2024-04-21 23:18:10 +0200

    Handle routes

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 
+	// Register routes
+	http.HandleFunc("/", basicHandler)
+	http.HandleFunc("/hello", handleHello)
+
 	// Create an HTTP server with the specified address and router as the handler
 	server := &http.Server{
-		Addr:    ":2020",
-		Handler: http.HandlerFunc(basicHandler),
+		Addr: ":2020",
 	}
 
 	// Start the server and listen for incoming requests
@@ -22,8 +25,12 @@ func main() {
 	}
 }
 
-// handle all requests
 // Respond to HTTP requests with for example a message
 func basicHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to the home page!"))
+}
+
+// Respond to HTTP requests with for example a message
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
