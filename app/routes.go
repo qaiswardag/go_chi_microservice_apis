502b9e305e3ae490a8cf53ca94482ab25e9cbb7a
Qais Wardag <qais.wardag@outlook.com>
2024-04-22 22:54:37 +0200

    Refactor code and routes

--- app/routes.go
+++ app/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/qaiswardag/go_chi_microservice_apis/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -14,7 +15,20 @@ func loadRoutes() *chi.Mux {
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to the API"))
 	})
 
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
---
+++ handler/order.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Order struct {
+}
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create an order")
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("List all orders")
+	w.Write([]byte("List of all orders"))
+}
+
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get an order by ID")
+}
+
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update an order by ID")
+}
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete an order by ID")
+}
